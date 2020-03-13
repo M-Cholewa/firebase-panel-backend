e1ed9449723657c5f261c3e35078c30d5f80f872
BuildTools <unconfigured@null.spigotmc.org>
2020-03-13 13:14:52 +0100

    form validation implemented

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express'),
     cors = require('cors'),
     admin = require('firebase-admin'),
     fs = require('fs'),
-    multer = require('multer'),
+    verifyForm = require('./classes/FormVerifier'),
     cookieParser = require('cookie-parser');
 
 
@@ -95,68 +95,57 @@ app.post('/getDB', verifyUser, (req, res) => {
 // var upload = multer({
 //     dest: 'uploads/',
 // })
-var bucket = admin.storage().bucket('gterenowa.appspot.com')
 // console.log(bucket)
 
-var upload = multer({ dest: 'uploads/', })
-// var upload = multer({ storage: multer.memoryStorage() })
-app.post('/submitForm', verifyUser, upload.single('image'), (req, res) => {
+
+var bucket = admin.storage().bucket('gterenowa.appspot.com')
+app.post('/submitForm', verifyUser, verifyForm, (req, res) => {
     let key = req.body.key
-    // console.log(key + " KEY")
-    // console.log(JSON.parse(req.body.dobreOdpowiedzi) + " odpowiedzi")
-    if (req.file) {
-        if (req.file.mimetype.includes('image/')) {
-            const options = {
-                destination: "game_imgs/" + key + ".jpg",
-                resumable: true,
-                validation: 'crc32c',
-                metadata: {
-                    contentType: req.file.mimetype,
-                }
-            };
-            bucket.upload('uploads/' + req.file.filename, options, function (err, file) {
-                // console.log(file.metadata.mediaLink)
-                if (err)
-                    res.status(422).send(err)
-                else
-                    file.makePublic().then(function (data) {
-                        // console.log(file)
-                        console.log(file.metadata.mediaLink)
-                        updateDB(req.body, file.metadata.mediaLink, key)
-                            .then(response => {
-                                // console.log(response)
-                                res.status(200).send('Wysłano pomyślnie')
-                            })
-                            .catch(err => {
-                                res.status(500).send('Błąd serwera')
+    res.status(200).send("git plik")
 
-                            })
-                        // console.log(data[0]);
-                    });
-                // console.log('Uploaded a blob or file!');
-                // console.log('link: ');
-                fs.unlink('uploads/' + req.file.filename, function (err) {
-                    if (err) throw err;
-                    // console.log('File deleted!');
-                });
-            });
-        }
-        else {
-            res.status(422).send('Nieprawidlowy format. Musi byc obrazek')
-        }
-    } else {
-        updateDB(req.body, null, key)
-            .then(() => {
-                // console.log(response)
-                res.status(200).send('Wysłano pomyślnie')
-            })
-            .catch(err => {
-                res.status(500).send('Błąd serwera')
+    // if (req.file) {
+    //     if (req.file.mimetype.includes('image/')) {
+    //         const options = {
+    //             destination: "game_imgs/" + key + ".jpg",
+    //             resumable: true,
+    //             validation: 'crc32c',
+    //             metadata: {
+    //                 contentType: req.file.mimetype,
+    //             }
+    //         };
+    //         bucket.upload('uploads/' + req.file.filename, options, function (err, file) {
+    //             if (err)
+    //                 res.status(422).send(err)
+    //             else
+    //                 file.makePublic().then(function (data) {
+    //                     console.log(file.metadata.mediaLink)
+    //                     updateDB(req.body, file.metadata.mediaLink, key)
+    //                         .then(response => {
+    //                             res.status(200).send('Wysłano pomyślnie')
+    //                         })
+    //                         .catch(err => {
+    //                             res.status(500).send('Błąd serwera')
 
-            })
-        // res.status(200).send('Wysłano pomyślnie bez obrazka')
+    //                         })
+    //                 });
+    //             fs.unlink('uploads/' + req.file.filename, function (err) {
+    //                 if (err) throw err;
+    //             });
+    //         });
+    //     }
+    //     else {
+    //         res.status(422).send('Nieprawidlowy format. Musi byc obrazek')
+    //     }
+    // } else {
+    //     updateDB(res, key)
+    //         .then(() => {
+    //             res.status(200).send('Wysłano pomyślnie')
+    //         })
+    //         .catch(err => {
+    //             res.status(500).send('Błąd serwera')
 
-    }
+    //         })
+    // }
 })
 
 app.post('/removeTask', verifyUser, (req, res) => {
@@ -174,21 +163,21 @@ app.post('/checkLogged', verifyUser, (req, res) => {
     res.status(200).send('logged in successfully')
 })
 
-const updateDB = (req, imgLink, key) => {
-    let czyWymagane = Boolean(req.czyWymagane)
-    let dobreOdpowiedzi = JSON.parse(req.dobreOdpowiedzi)
-    let lokalizacjaDl = Number(req.lokalizacjaDl)
-    let lokalizacjaSzer = Number(req.lokalizacjaSzer)
-    let podpisObrazka = req.podpisObrazka
-    let podtytul = req.podtytul
-    let trescZadania = req.trescZadania
-    let wprowadzenieDoZadania = req.wprowadzenieDoZadania
-    let tytul = req.tytul
+const updateDB = (req, res, key, imgLink) => {
+    // let czyWymagane = Boolean(req.czyWymagane)
+    // let dobreOdpowiedzi = JSON.parse(req.dobreOdpowiedzi)
+    // let lokalizacjaDl = Number(req.lokalizacjaDl)
+    // let lokalizacjaSzer = Number(req.lokalizacjaSzer)
+    // let podpisObrazka = req.podpisObrazka
+    // let podtytul = req.podtytul
+    // let trescZadania = req.trescZadania
+    // let wprowadzenieDoZadania = req.wprowadzenieDoZadania
+    // let tytul = req.tytul
     let urlZdjeciaDoZadania = imgLink ? imgLink : req.image
     // console.log("URL: " + urlZdjeciaDoZadania)
-    return admin.database().ref('zadanie/' + key).set({
-        czyWymagane, dobreOdpowiedzi, lokalizacjaDl, lokalizacjaSzer, podpisObrazka, podtytul, trescZadania, tytul, urlZdjeciaDoZadania, wprowadzenieDoZadania
-    });
+    let form = res.locals.form
+    form = Object.assign({ urlZdjeciaDoZadania }, form)
+    return admin.database().ref('zadanie/' + key).set(form);
 }
 
 const sendMessage = (title, message) => {
---
+++ classes/FormVerifier.js
@@ -0,0 +1,90 @@
+const multer = require('multer'),
+    validate = require('validate.js');
+
+const upload = multer({ dest: 'uploads/', limits: { fileSize: 1048576 } }).single('image')
+module.exports = verifyForm = (req, res, next) => {
+    upload(req, res, err => {
+        // console.log(err)
+        if (err)
+            res.status(500).send(err.message)
+        else {
+            let czyWymagane = Boolean(req.body.czyWymagane)
+            let dobreOdpowiedzi = JSON.parse(req.body.dobreOdpowiedzi)
+            let lokalizacjaDl = Number(req.body.lokalizacjaDl)
+            let lokalizacjaSzer = Number(req.body.lokalizacjaSzer)
+            let podpisObrazka = req.body.podpisObrazka
+            let podtytul = req.body.podtytul
+            let trescZadania = req.body.trescZadania
+            let wprowadzenieDoZadania = req.body.wprowadzenieDoZadania
+            let tytul = req.body.tytul
+            // let urlZdjeciaDoZadania = imgLink ? imgLink : req.body.image
+            let form = {
+                czyWymagane, dobreOdpowiedzi, lokalizacjaDl, lokalizacjaSzer,
+                podpisObrazka, podtytul, trescZadania, wprowadzenieDoZadania, tytul
+            }
+
+            let resValidate = validate(form, constraints, { format: "flat" })
+            if (resValidate) {
+                res.status(422).send(resValidate)
+                return
+            }
+
+            res.locals.form = form
+            next()
+        }
+    })
+}
+
+const stringType = {
+    presence: {
+        presence: true,
+        message: "nie podano żadnej wartości"
+    },
+    type: {
+        type: "string",
+        message: "zły typ danych"
+    },
+}
+const numberType = {
+    presence: {
+        presence: true,
+        message: "nie podano żadnej wartości"
+    },
+    type: {
+        type: "number",
+        message: "zły typ danych"
+    },
+}
+
+const constraints = {
+    czyWymagane: {
+        presence: {
+            presence: true,
+            message: "^Nie podano czy zadanie jest wymagane"
+        },
+        type: {
+            type: "boolean",
+            message: "zły typ danych"
+        }
+    },
+    dobreOdpowiedzi: {
+        presence: {
+            presence: true,
+            message: "^Nie podano żadnej dobrej odpowiedzi"
+        },
+        type: {
+            type: "array",
+            message: "zły typ danych"
+        },
+        length: {
+            minimum: 1,
+            message: "^Kazde zadanie musi mieć minimum 1 dobrą odpowiedź"
+        }
+    },
+    lokalizacjaDl: numberType,
+    lokalizacjaSzer: numberType,
+    podtytul: stringType,
+    trescZadania: stringType,
+    wprowadzenieDoZadania: stringType,
+    tytul: stringType,
+}
